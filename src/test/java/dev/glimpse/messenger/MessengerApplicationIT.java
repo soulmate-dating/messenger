f56faffa2f5978c07aebbdfb52067524e1ac5638
Dmitry Kutuzov <archie-woc@ya.ru>
2024-02-18 20:44:13 +0300

    Configure integration test
    Configure integration test with cassandra test container
    
    JIRA ID: TIN-29

---
+++ src/test/java/dev/glimpse/messenger/MessengerApplicationIT.java
@@ -0,0 +1,14 @@
+package dev.glimpse.messenger;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@IntegrationTest
+class MessengerApplicationIT {
+
+    @DisplayName("Check that the application context loads")
+    @Test
+    void contextLoads() {
+    }
+
+}
