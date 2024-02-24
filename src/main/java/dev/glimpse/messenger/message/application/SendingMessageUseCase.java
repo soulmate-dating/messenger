e2e82fdf0f211885428d5cad05dbb86a8668652a
Dmitry Kutuzov <archie-woc@ya.ru>
2024-02-24 23:29:17 +0300

    Implement send message endpoint
    Implement send message endpoint
    
    JIRA ID: TIN-32

---
+++ src/main/java/dev/glimpse/messenger/message/application/SendingMessageUseCase.java
@@ -0,0 +1,28 @@
+package dev.glimpse.messenger.message.application;
+
+import dev.glimpse.messenger.message.entity.Message;
+import dev.glimpse.messenger.message.entity.MessageContent;
+import dev.glimpse.messenger.user.entity.Recipient;
+import dev.glimpse.messenger.user.entity.Sender;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class SendingMessageUseCase {
+
+    private final MessageRepository messageRepository;
+
+    public Message execute(@NonNull UUID senderId,
+                           @NonNull UUID recipientId,
+                           @NonNull MessageContent content) {
+        Sender sender = Sender.of(senderId);
+        Recipient recipient = Recipient.of(recipientId);
+        Message message = Message.of(sender, recipient, content);
+        return messageRepository.save(message);
+    }
+
+}
---
+++ src/test/java/dev/glimpse/messenger/message/adapter/converter/MessageToMessageDtoConverterTest.java
@@ -0,0 +1,33 @@
+package dev.glimpse.messenger.message.adapter.converter;
+
+import dev.glimpse.messenger.message.entity.MessageObjectMother;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.ZoneOffset;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class MessageToMessageDtoConverterTest {
+
+    @DisplayName("Check that the message is converted to message dto.")
+    @Test
+    public void testMessageToMessageDtoConversion() {
+        // Arrange
+        var message = MessageObjectMother.createMessage();
+        var sut = new MessageToMessageDtoConverter();
+
+        // Act
+        var messageDto = sut.convert(message);
+
+        // Assert
+        assertNotNull(messageDto);
+        assertEquals(message.getId().getValue(), messageDto.getId());
+        assertEquals(message.getSender().getId(), messageDto.getSenderId());
+        assertEquals(message.getRecipient().getId(), messageDto.getRecipientId());
+        assertEquals(message.getSentAt().getValue().atOffset(ZoneOffset.UTC), messageDto.getDate());
+        assertEquals(message.getContent().getValue(), messageDto.getContent());
+    }
+
+}
