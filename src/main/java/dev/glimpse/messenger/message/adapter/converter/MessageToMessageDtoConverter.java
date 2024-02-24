a60a43bb61ab0058024d8b20650ad87d9f915afb
Dmitry Kutuzov <archie-woc@ya.ru>
2024-02-24 23:41:45 +0300

    Add recipient id for getting messages
    Add recipient id for getting messages
    
    JIRA ID: TIN-32

--- src/main/java/dev/glimpse/messenger/message/adapter/converter/MessageToMessageDtoConverter.java
+++ src/main/java/dev/glimpse/messenger/message/adapter/converter/MessageToMessageDtoConverter.java
@@ -2,6 +2,7 @@ package dev.glimpse.messenger.message.adapter.converter;
 
 import dev.glimpse.messenger.message.entity.Message;
 import dev.glimpse.messenger.message.presentation.dto.MessageDto;
+import lombok.NonNull;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,7 @@ import java.time.ZoneOffset;
 @Component
 public class MessageToMessageDtoConverter implements Converter<Message, MessageDto> {
     @Override
-    public MessageDto convert(Message source) {
+    public MessageDto convert(@NonNull Message source) {
         return new MessageDto()
                 .id(source.getId().getValue())
                 .senderId(source.getSender().getId())
