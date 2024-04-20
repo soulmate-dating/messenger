64b037548a9d8c7f015916e16415d88fa76bc036
Dmitry Kutuzov <archie-woc@ya.ru>
2024-04-20 18:30:24 +0300

    Fix allowed origins for ws
    Fix allowed origins for ws
    
    JIRA ID: TIN-00
    
    Signed-off-by: Dmitry Kutuzov <archie-woc@ya.ru>

--- src/main/java/dev/glimpse/messenger/config/WebSocketConfig.java
+++ src/main/java/dev/glimpse/messenger/config/WebSocketConfig.java
@@ -28,6 +28,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
